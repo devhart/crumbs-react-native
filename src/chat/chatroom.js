5f4ea0fcd1d4c6632291d11fa709939eca17ff99
hchsquared <hannahc.henderson@gmail.com>
2016-07-27 21:40:42 -0700

    Commit to allow PR of main.js

--- src/chat/chatroom.js
+++ src/chat/chatroom.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   ListView,
   TouchableHighlight,
+  Dimensions,
   StyleSheet
 } from 'react-native';
 
@@ -14,50 +15,112 @@ const PULLDOWN_DISTANCE = 40;
 export default class Chatroom extends Component {
   constructor(props) {
 
-    super(props);
+    super(props); // provides access to props.socket
 
     this.state = {
-      messages: null,
+      message: null,
       location: '37.7837-122.4090',
       demoMode: true,
       userLoggedIn: false,
     }
 
-    props.socket.on('test from server', () => console.log('xxxx client received server test'));
-    console.log('props are:', props);
   }
 
+  componentWillMount() {
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  // componentWillMount() {
-  //   this.getChannelList(1);
-  // }
+  handleInputChange(text) {
+    this.setState({
+      message: text,
+    });
+  }
 
-  // onChatroomPress(url) {
-  //   console.log(url);
-  // }
+  handleSubmit() {
+    // ADD MESSAGE TO CHAT ROOM
+    props.socket.emit('addMessage', this.message);
+    console.log('addMessage:', this.message);
 
-  // getChatroomList(page) {
-  //   if (page == 0) {
-  //     return;
-  //   }
-  //   this.socket.emit('getChats', 'no data')
-  // }
+    this.setState({
+      message: '',
+    })
+
+  }
+
+  onBackPress() {
+    console.log('navigate back a page');
+  }
+
+// DUPLICATES HANDLE SUBMIT
+  onSendPress() {
+    console.log(this.state.message);
+    this.setState({message: ''});
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Chatroom Take 2</Text>
-        <Text>{this.state.location}</Text>
+        <View style={styles.topContainer}>
+          <TouchableHighlight
+            underlayColor={'blue'}
+            onPress={this.onBackPress}
+          >
+          <Text>Back</Text>
+          </TouchableHighlight>
+        </View>
+        <View>
+          <Text>CHAT</Text>
+        </View>
+        <View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={this.message}
+              onChangeText={(text) => this.handleInputChange(text)}
+              />
+          </View>
+          <View> 
+            <TouchableHighlight
+              underlayColor={'red'}
+              onPress={() => this.onSendPress()}
+              >
+              <Text>SEND</Text>
+            </TouchableHighlight>
+          </View> 
+        </View>
       </View>
     );
   }
 }
+
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#ffffff'
+  },
+  topContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 20,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'blue'
   }
+
 });
+
+
+      // <View style={styles.container}>
+      //   <Text>Chatroom Take 2</Text>
+      //   <Text>{this.state.location}</Text>
+      // </View>
