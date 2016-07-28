39f20a2b3072be1e6bb3a95590009108efac2fe2
wayneadams <wadams19@gmail.com>
2016-07-27 20:47:48 -0700

    Add basic navigation button to chat room

--- src/map/map.js
+++ src/map/map.js
@@ -3,22 +3,44 @@ import {
   View,
   StyleSheet,
   MapView,
+  TouchableHighlight,
+  Text
 } from 'react-native';
 
 export default class Login extends Component {
   render() {
     return (
+      <View style={styles.container}>
         <MapView
           style={styles.map}
           showsUserLocation={true}
           followUserLocation={true}
         />
+        <TouchableHighlight
+          style={ styles.button }
+          onPress={ () => console.log('Navigate to the chat room') }>
+          <Text style={ styles.buttonText }>Enter Chat Room</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  map: {
-    flex: 1,
+  container: {
+    flex: 1
   },
+  map: {
+    flex: 1
+  },
+  button: {
+  	height:60,
+    justifyContent: 'center',
+    backgroundColor: '#efefef',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+  	fontSize:20
+  }
 });
